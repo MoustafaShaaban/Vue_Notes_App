85f93db4b73dc847bd757ee7eed04ff9141c4b0f
Moustafa Shaaban <57029110+MoustafaShaaban@users.noreply.github.com>
2024-04-20 00:11:21 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -31,15 +31,6 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
-      path: "/edit-note/:id/:title/:content",
-      name: "editNote",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../components/NoteEdit.vue"),
-      props: true,
-    },
-    {
       path: "/:pathMatch(.*)*",
       name: "NotFound",
       component: NotFound,
